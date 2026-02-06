fc598db53cfe18f28dd53313ab0da9f5aaf36ace
shauncode24 <shaunak.karve@somaiya.edu>
2026-02-06 14:31:12 +0530

    Homepage bg set

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
 import './Homepage.css';
 import bg from '@/assets/bg_14.png';
+import fireplace from '@/assets/fireplace_2.gif';
 
 export default function Homepage() {
     return (
         <div className="homepage-container">
             <img className="homepage-background" src={bg} alt="Chest" />
+            <img className="homepage-fireplace" src={fireplace} alt="Fireplace" />
+            <div className="homepage-content">
+                Shaunak Karve
+            </div>
         </div>
     );
 }
